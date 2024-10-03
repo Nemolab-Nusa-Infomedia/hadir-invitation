f3e7963c746580b84c1ec576afd24bbd39408a87
vindrayulian <vindrayulian@gmail.com>
2024-10-03 12:27:50 +0700

    style : new undangan

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -269,7 +269,7 @@
 
                 <!-- Card 3 (Khitanan) -->
                 <div class="template-item bg-white rounded-lg shadow-md overflow-hidden fade-enter fade-enter-active" data-category="khitanan">
-                    <img src="{{ asset('assets/img/mockup-undangan/frame3.png') }}" alt="Nuansa Nostalgia" class="w-full h-64 sm:h-80 object-cover">
+                    <img src="{{ asset('assets/img/mockup-undangan/frame4.png') }}" alt="Nuansa Nostalgia" class="w-full h-64 sm:h-80 object-cover">
                     <div class="p-2 sm:p-4 text-start">
                         <h3 class="text-sm sm:text-lg font-semibold mb-2">Khitan</h3>
                         <div class="flex justify-between gap-2">
@@ -292,6 +292,30 @@
                 </div>
 
                 <!-- Card 5 (Pernikahan) -->
+                <div class="template-item bg-white rounded-lg shadow-md overflow-hidden fade-enter fade-enter-active" data-category="pernikahan">
+                    <img src="{{ asset('assets/img/mockup-undangan/frame1.png') }}" alt="Khitanan Modern" class="w-full h-64 sm:h-80 object-cover">
+                    <div class="p-2 sm:p-4 text-start">
+                        <h3 class="text-sm sm:text-lg font-semibold mb-2">Wedding</h3>
+                        <div class="flex justify-between gap-2">
+                            <a href="https://nemolab-nusa-infomedia.github.io/template-invitation/view/undangan-v5/index.html" class="px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm bg-coklat text-white rounded-md">Lihat Demo</a>
+                            <button class="px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm bg-coklat text-white rounded-md">Pesan</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 6 (Pernikahan) -->
+                <div class="template-item bg-white rounded-lg shadow-md overflow-hidden fade-enter fade-enter-active" data-category="pernikahan">
+                    <img src="{{ asset('assets/img/mockup-undangan/frame1.png') }}" alt="Khitanan Modern" class="w-full h-64 sm:h-80 object-cover">
+                    <div class="p-2 sm:p-4 text-start">
+                        <h3 class="text-sm sm:text-lg font-semibold mb-2">Wedding</h3>
+                        <div class="flex justify-between gap-2">
+                            <a href="https://nemolab-nusa-infomedia.github.io/template-invitation/view/undangan-v5/index.html" class="px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm bg-coklat text-white rounded-md">Lihat Demo</a>
+                            <button class="px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm bg-coklat text-white rounded-md">Pesan</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 7 (Pernikahan) -->
                 <div class="template-item bg-white rounded-lg shadow-md overflow-hidden fade-enter fade-enter-active" data-category="pernikahan">
                     <img src="{{ asset('assets/img/mockup-undangan/frame1.png') }}" alt="Khitanan Modern" class="w-full h-64 sm:h-80 object-cover">
                     <div class="p-2 sm:p-4 text-start">
