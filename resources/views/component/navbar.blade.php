1608d6f1268a5a749ad7312e717a0f77e9a92730
vindrayulian <vindrayulian@gmail.com>
2024-10-11 11:16:53 +0700

    style : add menu blog

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -17,6 +17,9 @@
         <li class="nav-item">
           <a class="nav-link" href="#template">Template</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://blog.hadir.web.id">Blog</a>
+        </li>
         <!-- <li class="nav-item">
             <a href="#" class="btn btn-outline-pink text-dark">Masuk</a>
         </li> -->
@@ -29,10 +32,11 @@
 
   <div class="mobile-bottom-nav shadow">
     <ul class="bottom-nav">
-      <li><a href="#home"><i class="fa-solid fa-house"></i><span>Beranda</span></a></li>
-      <li><a href="#keunggulan"><i class='bx bx-calendar-star'></i><span>Keunggulan</span></a></li>
+      <li><a href="#home"><i class='bx bx-home'></i><span>Beranda</span></a></li>
+      {{-- <li><a href="#keunggulan"><i class='bx bx-calendar-star'></i><span>Keunggulan</span></a></li> --}}
       <li><a href="#bantuan"><i class='bx bx-help-circle'></i><span>Bantuan</span></a></li>
-      <li><a href="#template"><i class='bx bx-folder-open'></i></i><span>Template</span></a></li>
+      <li><a href="#template"><i class='bx bx-folder-open'></i><span>Template</span></a></li>
+      <li><a href="https://blog.hadir.web.id"><i class='bx bx-news' ></i><span>Blog</span></a></li>
       <li><a href="#template"><i class='bx bx-user'></i><span>Login</span></a></li>
     </ul>
   </div>
